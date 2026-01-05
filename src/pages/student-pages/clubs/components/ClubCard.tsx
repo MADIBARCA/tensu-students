fb699db90ea28afbd25f55f6b6e9cadbd2c37a33
Madi Abzhanov <abzhanma@gmail.com>
2026-01-05 05:47:43 +0500

    add

--- src/pages/student-pages/clubs/components/ClubCard.tsx
+++ src/pages/student-pages/clubs/components/ClubCard.tsx
@@ -63,7 +63,7 @@ export const ClubCard: React.FC<ClubCardProps> = ({ club, isMember, onClick }) =
         
         {/* Logo - positioned to overlap cover and content */}
         <div className="absolute -bottom-7 left-4">
-          <div className="w-14 h-14 rounded-full ring-4 ring-white shadow-xl overflow-hidden bg-white">
+          <div className="w-14 h-14 rounded-full ring-4 ring-white shadow-xl overflow-hidden bg-white border-gray-500 border">
             {club.logo_url ? (
               <img 
                 src={club.logo_url} 
